e95713e0a7f7340edbfa797975f4225f70ddb76d
joaopinheiro4423 <101719441+joaopinheiro4423@users.noreply.github.com>
2022-03-23 07:03:07 -0300

    Create codigo.ino

---
+++ exercicio 2/codigo.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+
+}
